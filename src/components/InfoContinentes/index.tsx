5f8559dcb617f2f047caf1a2555c14f84fe7b198
Gabriel Andrade <gabriel.santana@gmail.com>
2021-06-14 05:25:24 -0300

    Ajustando os cards

---
+++ src/components/InfoContinentes/index.tsx
@@ -0,0 +1,126 @@
+import {Flex,Box,Text,HStack, Wrap, WrapItem,Center,Image} from '@chakra-ui/react';
+
+export default function InfoContinentes (){
+    return (
+        <>
+            <Flex flexDirection="row" alignItems="center" justifyContent="space-between" w="70%">
+                <Box h="211px" w="600px">
+                    <Text textAlign="justify">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting 
+                        industry. Lorem Ipsum has been the industry's standard dummy text ever
+                        since the 1500s, when an unknown printer took a galley of type and scrambled 
+                        it to make a type specimen book. It has survived not only five centuries, but 
+                        also the leap into electronic typesetting, remaining essentially unchanged. It
+                        was popularised in the 1960s with the release of Letraset sheets containing Lorem
+                        Ipsum passages, and more recently with desktop publishing 
+                        software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </Text>
+                </Box>
+                <Box  h="99px" w="490px"  alignItems="center" justifyContent="center">
+                    <HStack justifyContent="space-between"  >
+                        <Flex alignItems="center" justifyContent="center" flexDirection="column">
+                            <Text 
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                fontSize="48px"
+                                textAlign="center"
+                                textColor={'#FFBA08'} >
+                                50
+                            </Text>
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                fontSize="24px"
+                                textAlign="center"
+                            >países</Text>
+                        </Flex>
+                        <Flex alignItems="center" justifyContent="center" flexDirection="column">
+                            <Text 
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                fontSize="48px"
+                                textAlign="center"
+                                textColor={'#FFBA08'} >60
+                            </Text>
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                fontSize="24px"
+                                textAlign="center"
+                            >línguas</Text>
+                        </Flex>
+                        <Flex alignItems="center" justifyContent="center" flexDirection="column">
+                            <Text 
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                fontSize="48px"
+                                textAlign="center"
+                                textColor={'#FFBA08'} >27
+                            </Text>
+                            <Text
+                                fontFamily="Poppins"
+                                fontWeight="600"
+                                fontSize="24px"
+                                textAlign="center"
+                            >cidades + 100</Text>
+                        </Flex>
+                    </HStack>
+                </Box>
+            </Flex>
+            <Flex h="700"  bg="tomato"  w="70%" flexDirection="column">
+                <Flex mb="10" justifyContent="flex-start" alignItems="flex-start">
+                    <Text
+                        fontFamily="Poppins"
+                        fontWeight="600"
+                        fontSize="24px"
+                        textAlign="center"
+                    >Cidades + 100</Text>
+                </Flex>
+                <Wrap spacing="50px">
+                    <WrapItem>
+                        <Center w="256px" h="279px" bg="red.200" borderRadius={5}>
+                          <Flex w="100%" h="100%"  flexDir="column">
+                            <Image src={'/images/LondonFoto.svg'} h="173px" w="256px" />
+                            <Flex flexDirection="row" h="100%">
+                                <Flex bg="blue.100" 
+                                      w="50%"
+                                      justifyContent="space-around"
+                                      flexDirection="column">
+                                    <Text>Londres</Text>
+                                    <Text>Reino Unido</Text>
+                                </Flex>
+                                <Flex h="100%" w="50%" align="center" justifyContent="center">
+                                    <Image src={'/images/EllipseEngland.svg'} h="10" w="10"/>
+                                </Flex>
+                            </Flex>
+                            
+                          </Flex>
+                         
+                        </Center>
+                    </WrapItem>
+                    <WrapItem>
+                        <Center w="256px"  h="279px" bg="green.200">
+                            <Image h="173px" w="256px" />
+                        </Center>
+                    </WrapItem>
+                    <WrapItem>
+                        <Center w="256px"  h="279px" bg="tomato">
+                            <Image h="173px" w="256px" />
+                        </Center>
+                    </WrapItem>
+                    <WrapItem>
+                        <Center w="256px"  h="279px" bg="blue.200">
+                            <Image h="173px" w="256px" />
+                        </Center>
+                    </WrapItem>
+                    <WrapItem>
+                        <Center w="256px"  h="279px" bg="blackAlpha.500">
+                            <Image h="173px" w="256px" />
+                        </Center>
+                    </WrapItem>
+                </Wrap>
+            </Flex>
+        </>
+
+    )
+}
--- src/pages/Continente/index.tsx
+++ src/pages/Continente/index.tsx
@@ -1,7 +1,9 @@
 import {Box,VStack} from '@chakra-ui/react';
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
-export default function Content(){
+import InfoContinentes from '../../components/InfoContinentes';
+
+export default function Continente(){
     const continente = [{continente:'europa',bunner:""}]
     return (
    
@@ -10,6 +12,7 @@ export default function Content(){
           <VStack spacing={12}>
             <Header />
             <Banner src="/images/Banner.svg" h={500} w="100%"/>
+            <InfoContinentes />
           </VStack>
           </Box>
         </>
